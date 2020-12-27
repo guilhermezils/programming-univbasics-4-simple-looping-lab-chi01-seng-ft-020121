b0a27361acf11fbe3f71a3cb2a6b033b2d40d25d
Guilherme Ehrat Zils <guilhermezils@icloud.com>
2020-12-27 22:07:48 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,19 @@
 # Write your methods here
+
+require 'pry'
+
+def loop_message_five_times (message = "Hello World.")
+   count = 0 
+   while count < 5 do
+   puts message
+   count += 1
+  end
+
+end
+
+
+def loop_message_n_times(message, number)
+  puts #{message}*#{number} 
+ 
+end
+
